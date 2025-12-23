c199f9a42968f80e9d6b100800bbb50318167904
MaGnsi0 <magnsi0o0@gmail.com>
2025-12-23 16:31:59 +0200

    cleanup

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,6 +2,7 @@ package request
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -11,44 +12,67 @@ type Request struct {
 }
 
 type RequestLine struct {
-	HttpVersion   string
+	HTTPVersion   string
 	RequestTarget string
 	Method        string
 }
 
+func (rl *RequestLine) ValidMethod() bool {
+	return rl.Method == strings.ToUpper(rl.Method)
+}
+
+func (rl *RequestLine) ValidHTTP() bool {
+	return rl.HTTPVersion == "1.1"
+}
+
+var SEPARATOR = "\r\n"
+var (
+	ErrorMalformedRequestLine = fmt.Errorf("malformed request-line")
+	ErrorInvalidMethod        = fmt.Errorf("invalid method")
+	ErrorInvalidHTTPVersion   = fmt.Errorf("invalid HTTP version")
+)
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	r, err := io.ReadAll(reader)
+	data, err := io.ReadAll(reader)
 
 	if err != nil {
 		return nil, err
 	}
 
-	requestLines := strings.Split(string(r), "\r\n")
+	rl, err := parseRequestLine(string(data))
 
-	if len(requestLines) < 1 {
-		return nil, errors.New("empty request")
-	}
-
-	requestLine := requestLines[0]
-	requestLineParts := strings.Split(requestLine, " ")
-
-	if len(requestLineParts) != 3 {
-		return nil, errors.New("invalid request line")
-	}
-
-	if requestLineParts[0] != strings.ToUpper(requestLineParts[0]) {
-		return nil, errors.New("invalid request method")
-	}
-
-	if requestLineParts[2] != "HTTP/1.1" {
-		return nil, errors.New("invalid http version")
+	if err != nil {
+		return nil, err
 	}
 
 	return &Request{
-		RequestLine: RequestLine{
-			Method:        requestLineParts[0],
-			RequestTarget: requestLineParts[1],
-			HttpVersion:   strings.TrimPrefix(requestLineParts[2], "HTTP/"),
-		},
+		RequestLine: *rl,
 	}, nil
+}
+
+func parseRequestLine(r string) (*RequestLine, error) {
+	requestLines := strings.Split(r, SEPARATOR)
+	if len(requestLines) == 0 {
+		return nil, errors.New("empty request")
+	}
+
+	parts := strings.Split(requestLines[0], " ")
+	if len(parts) != 3 {
+		return nil, ErrorMalformedRequestLine
+	}
+
+	rl := &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HTTPVersion:   strings.TrimPrefix(parts[2], "HTTP/"),
+	}
+
+	if !rl.ValidMethod() {
+		return nil, ErrorInvalidMethod
+	}
+	if !rl.ValidHTTP() {
+		return nil, ErrorInvalidHTTPVersion
+	}
+
+	return rl, nil
 }
